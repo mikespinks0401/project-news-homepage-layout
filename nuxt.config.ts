6f8fdffa5740a302abaf73ca66afb282ad93ab07
M.Spinks <mikespinks0401@gmail.com>
2023-03-24 08:57:08 -0500

    include defaults

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
     css: ["vuetify/lib/styles/main.sass", "@mdi/font/css/materialdesignicons.css"], 
     ssr: false,
+    app:{
+        baseURL:"/",
+        cdnURL: ""
+    },
     build:{
         transpile:["vuetify"]
     },
